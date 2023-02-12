2b4836feb065e27a7994d806fbc8f77baa8f3bf0
Alberto <25537372+albertobas@users.noreply.github.com>
2023-02-12 10:54:59 +0100

    refactor: use SSR components

--- next-app/src/pages/explore/certificates.tsx
+++ next-app/src/pages/explore/certificates.tsx
@@ -5,70 +5,70 @@
  *   the page per request at most once every `revalidateInterval` seconds on server side.
  */
 
-// import { GetServerSideProps } from 'next';
-// import { getBuiltGraphSDK } from 'next-app/.graphclient';
-// import Breadcrumbs from 'next-app/src/features/shared/ui/breadcrumbs/Breadcrumbs';
-// import Intro from 'next-app/src/features/shared/ui/intro/Intro';
-// import AllCertificates from 'next-app/src/features/explore/ui/certificates/AllCertificates';
-// import { brandName } from 'next-app/src/shared/utils/constants';
-// import allCertificatesAdapter from 'next-app/src/features/explore/core/adapters/allCertificates.adapter';
-// import { ICertificatesState, ICertificatesStateData } from 'next-app/src/features/shared/utils/interfaces';
-// import { getTime } from 'next-app/src/features/shared/utils/helpers/helpers';
-
-// function CertificatesPageSSR(state: ICertificatesState): JSX.Element {
-//   return (
-//     <>
-//       <Breadcrumbs />
-//       <Intro
-//         title="Certificates"
-//         description={`In this page you can explore all the certificates that exist in ${brandName}.`}
-//         note="This page updates per request."
-//       />
-//       <AllCertificates {...state} />
-//     </>
-//   );
-// }
-
-// const sdk = getBuiltGraphSDK();
-
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const lastUpdated = getTime();
-//   try {
-//     const dataRaw = await sdk.AllCertificates();
-//     const data: ICertificatesStateData = { certificates: allCertificatesAdapter(dataRaw), lastUpdated };
-//     return { props: { error: false, data } };
-//   } catch (e) {
-//     const data: ICertificatesStateData = { certificates: null, lastUpdated };
-//     return { props: { error: true, data } };
-//   }
-// };
-
-// export default CertificatesPageSSR;
-
-import { NextPage } from 'next';
+import { GetServerSideProps } from 'next';
+import { getBuiltGraphSDK } from 'next-app/.graphclient';
 import Breadcrumbs from 'next-app/src/features/shared/ui/breadcrumbs/Breadcrumbs';
 import Intro from 'next-app/src/features/shared/ui/intro/Intro';
-import { useAllCertificates } from 'next-app/src/features/explore/ui/hooks/useAllCertificates';
-import { pollInterval } from 'next-app/src/features/shared/utils/constants';
 import AllCertificates from 'next-app/src/features/explore/ui/certificates/AllCertificates';
 import { brandName } from 'next-app/src/shared/utils/constants';
+import allCertificatesAdapter from 'next-app/src/features/explore/core/adapters/allCertificates.adapter';
+import { ICertificatesState, ICertificatesStateData } from 'next-app/src/features/shared/utils/interfaces';
+import { getTime } from 'next-app/src/features/shared/utils/helpers/helpers';
 
-const CertificatesPageCSR: NextPage = () => {
-  const state = useAllCertificates(pollInterval);
-
+function CertificatesPageSSR(state: ICertificatesState): JSX.Element {
   return (
     <>
       <Breadcrumbs />
       <Intro
         title="Certificates"
         description={`In this page you can explore all the certificates that exist in ${brandName}.`}
+        note="This page updates per request."
       />
       <AllCertificates {...state} />
     </>
   );
+}
+
+const sdk = getBuiltGraphSDK();
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const lastUpdated = getTime();
+  try {
+    const dataRaw = await sdk.AllCertificates();
+    const data: ICertificatesStateData = { certificates: allCertificatesAdapter(dataRaw), lastUpdated };
+    return { props: { error: false, data } };
+  } catch (e) {
+    const data: ICertificatesStateData = { certificates: null, lastUpdated };
+    return { props: { error: true, data } };
+  }
 };
 
-export default CertificatesPageCSR;
+export default CertificatesPageSSR;
+
+// import { NextPage } from 'next';
+// import Breadcrumbs from 'next-app/src/features/shared/ui/breadcrumbs/Breadcrumbs';
+// import Intro from 'next-app/src/features/shared/ui/intro/Intro';
+// import { useAllCertificates } from 'next-app/src/features/explore/ui/hooks/useAllCertificates';
+// import { pollInterval } from 'next-app/src/features/shared/utils/constants';
+// import AllCertificates from 'next-app/src/features/explore/ui/certificates/AllCertificates';
+// import { brandName } from 'next-app/src/shared/utils/constants';
+
+// const CertificatesPageCSR: NextPage = () => {
+//   const state = useAllCertificates(pollInterval);
+
+//   return (
+//     <>
+//       <Breadcrumbs />
+//       <Intro
+//         title="Certificates"
+//         description={`In this page you can explore all the certificates that exist in ${brandName}.`}
+//       />
+//       <AllCertificates {...state} />
+//     </>
+//   );
+// };
+
+// export default CertificatesPageCSR;
 
 // import { GetStaticProps } from 'next';
 // import { getBuiltGraphSDK } from 'next-app/.graphclient';
