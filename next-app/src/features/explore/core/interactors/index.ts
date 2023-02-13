9a3100e60642ab316857231a712f0002a8085f08
Alberto <25537372+albertobas@users.noreply.github.com>
2023-02-13 19:58:36 +0100

    refactor: rename interactor files

--- next-app/src/features/explore/core/interactors/index.ts
+++ next-app/src/features/explore/core/interactors/index.ts
@@ -2,13 +2,13 @@ import CertifiatesDataSource from 'next-app/src/features/explore/dataSources/cer
 import EscrowsDataSource from 'next-app/src/features/explore/dataSources/escrows.datasource';
 import TokensDataSource from 'next-app/src/features/explore/dataSources/tokens.datasource';
 import TokenTypesDataSource from 'next-app/src/features/explore/dataSources/tokenTypes.datasource';
-import queryAllCertificates from 'next-app/src/features/explore/core/interactors/queryAllCertificates';
-import queryAllEscrows from 'next-app/src/features/explore/core/interactors/queryAllEscrows';
-import queryAllTokens from 'next-app/src/features/explore/core/interactors/queryAllTokens';
-import queryAllTokenTypes from 'next-app/src/features/explore/core/interactors/queryAllTokenTypes';
-import queryCertificateById from 'next-app/src/features/explore/core/interactors/queryCertificateById';
-import queryTokenById from 'next-app/src/features/explore/core/interactors/queryTokenById';
-import queryTokenTypeById from 'next-app/src/features/explore/core/interactors/queryTokenTypeById';
+import queryAllCertificates from 'next-app/src/features/explore/core/interactors/queryAllCertificates.interactor';
+import queryAllEscrows from 'next-app/src/features/explore/core/interactors/queryAllEscrows.interactor';
+import queryAllTokens from 'next-app/src/features/explore/core/interactors/queryAllTokens.interactor';
+import queryAllTokenTypes from 'next-app/src/features/explore/core/interactors/queryAllTokenTypes.interactor';
+import queryCertificateById from 'next-app/src/features/explore/core/interactors/queryCertificateById.interactor';
+import queryTokenById from 'next-app/src/features/explore/core/interactors/queryTokenById.interactor';
+import queryTokenTypeById from 'next-app/src/features/explore/core/interactors/queryTokenTypeById.interactor';
 
 const certificatesRepository = new CertifiatesDataSource();
 const escrowsRepository = new EscrowsDataSource();
--- next-app/src/features/management/core/interactors/index.ts
+++ next-app/src/features/management/core/interactors/index.ts
@@ -2,12 +2,12 @@ import MyCertificatesDataSource from 'next-app/src/features/management/dataSourc
 import MyEscrowsDataSource from 'next-app/src/features/management/dataSources/myEscrows.datasource';
 import MyTokensDataSource from 'next-app/src/features/management/dataSources/myTokens.datasource';
 import MyTokenTypesDataSource from 'next-app/src/features/management/dataSources/myTokenTypes.datasource';
-import queryAllTokenTypesAndCertificates from 'next-app/src/features/management/core/interactors/queryAllTokenTypesAndCertificates';
-import queryCertificatesByMemberAndAllTokenTypes from 'next-app/src/features/management/core/interactors/queryCertificatesByMemberAndAllTokenTypes';
-import queryEscrowsByBuyer from 'next-app/src/features/management/core/interactors/queryEscrowsByBuyer';
-import queryEscrowsByMember from 'next-app/src/features/management/core/interactors/queryEscrowsByMember';
-import queryTokensAndTokenTypesByMember from 'next-app/src/features/management/core/interactors/queryTokensAndTokenTypesByMember';
-import queryTokensByAccount from 'next-app/src/features/management/core/interactors/queryTokensByAccount';
+import queryAllTokenTypesAndCertificates from 'next-app/src/features/management/core/interactors/queryAllTokenTypesAndCertificates.interactor';
+import queryCertificatesByMemberAndAllTokenTypes from 'next-app/src/features/management/core/interactors/queryCertificatesByMemberAndAllTokenTypes.interactor';
+import queryEscrowsByBuyer from 'next-app/src/features/management/core/interactors/queryEscrowsByBuyer.interactor';
+import queryEscrowsByMember from 'next-app/src/features/management/core/interactors/queryEscrowsByMember.interactor';
+import queryTokensAndTokenTypesByMember from 'next-app/src/features/management/core/interactors/queryTokensAndTokenTypesByMember.interactor';
+import queryTokensByAccount from 'next-app/src/features/management/core/interactors/queryTokensByAccount.interactor';
 
 const myCertificatesRepository = new MyCertificatesDataSource();
 const myEscrowsRepository = new MyEscrowsDataSource();
